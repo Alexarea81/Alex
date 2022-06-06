3138c5b7444aca1e58ccb76a878330cd9563f748
Alex <alexkuceryb@gmail.com>
2022-06-06 19:21:28 +0300

    My first commit

---
+++ test.lesson.js
@@ -0,0 +1,9 @@
+const ex = require('express');
+const application = ex();
+const fs = require('fs');
+application.get("/stations", (request, response) => {
+ const stations = fs.readFileSync('./stations.json');
+ response.json(JSON.parse(stations));
+})
+
+application.listen(8081, () => console.log("ok"))
