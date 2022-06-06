0064d68e9a84c51def1425ba84f297959295acf3
Alex <alexkuceryb@gmail.com>
2022-06-06 19:38:55 +0300

    My thirst commit

--- test.lesson.js
+++ test.lesson.js
@@ -1,9 +1,16 @@
 const ex = require('express');
 const application = ex();
 const fs = require('fs');
+
+
 application.get("/stations", (request, response) => {
  const stations = fs.readFileSync('./stations.json');
  response.json(JSON.parse(stations));
 })
+
+application.get("/stations2", (request, response) => {
+    const stations = fs.readFileSync('./stations.json');
+    response.json(JSON.parse(stations));
+   })
 
 application.listen(8081, () => console.log("Good"))
