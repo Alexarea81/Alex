1ad948c904fd959247a437b745b32a5067649ef2
Alex <alexkuceryb@gmail.com>
2022-06-10 20:04:11 +0300

    del

--- test.lesson.js
+++ test.lesson.js
@@ -2,6 +2,7 @@
 const fs = require('fs.promises');
 const res = require("express/lib/response");
 const ex = require('express');
+const { param } = require("express/lib/request");
 const application = ex();
 
 application.use(ex.json())
@@ -13,7 +14,9 @@ application.get('/stations', (request, response) => {
 })
 
 application.post('/stations', (req, res) => {
+    console.log('here')
     const newStation = req.body;
+    console.log(newStation);
     fs.readFile('./stations.json').then(data => {
         const stationArray = JSON.parse(data);
         stationArray.push(newStation);
@@ -22,5 +25,35 @@ application.post('/stations', (req, res) => {
         })
     }).catch(err => console.error(err));
 });
-   
+
+
+application.delete('/stations/:id', (req, res) => {    
+    fs.readFile('./stations.json').then(data => {
+        const stationArray = JSON.parse(data);
+        console.log("id" ,req.params.id);
+        const newArray = stationArray.filter(s => s.id != req.params.id);
+        fs.writeFile('./stations.json', JSON.stringify(newArray)).then(() =>{
+            res.sendStatus(200);
+        })
+    }).catch(err => console.error(err));
+});
 application.listen(8085, () => console.log("Good Lesson"));
+
+application.get('/stations/:id', (request, response) => {
+    fs.readFile('./stations.json').then(stations => {
+        response.json(JSON.parse(stations).filter(s => s.id == request.params.id));
+
+    });
+});
+
+
+application.put('/stations/:id', (req, res) => {    
+    console.log(req.body)
+    fs.readFile('./stations.json').then(data => {
+        const stationArray = JSON.parse(data);
+
+        fs.writeFile('./stations.json', JSON.stringify(newArray)).then(() =>{
+            res.sendStatus(200);
+        })
+    }).catch(err => console.error(err));
+});
